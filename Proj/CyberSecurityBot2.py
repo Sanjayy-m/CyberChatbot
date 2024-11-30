2938c5758ea97a055aa912f4735e609d99b4e19c
Sanjayy-m <143164698+Sanjayy-m@users.noreply.github.com>
2024-11-30 23:54:04 +0530

    Update CyberSecurityBot2.py

--- Proj/CyberSecurityBot2.py
+++ Proj/CyberSecurityBot2.py
@@ -13,7 +13,7 @@ from vertexai.generative_models import (
     SafetySetting,
     GenerationConfig
 )
-
+from google.cloud import language
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_file("Proj/cred.json")
