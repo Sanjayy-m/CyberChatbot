40493105c26f5b446342a895b853ecfda5ee9644
Sanjayy-m <143164698+Sanjayy-m@users.noreply.github.com>
2024-11-30 21:33:03 +0530

    Update CyberSecurityBot2.py

--- Proj/CyberSecurityBot2.py
+++ Proj/CyberSecurityBot2.py
@@ -16,20 +16,16 @@ from vertexai.generative_models import (
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim import AdamW
 from transformers import AutoModel, AutoTokenizer
 from huggingface_hub import hf_hub_download
 
 # Initialize Tokenizers and Models
 bert_model_name = 'sanjayyy/newBert'
 simcse_model_name = 'sanjayyy/newSimCSE'
-@st.cache_resource
-def get_mod1():
-    tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
-    bert_model = AutoModel.from_pretrained(bert_model_name)
-    simcse_model = AutoModel.from_pretrained(simcse_model_name)
-    return tokenizer,bert_model,simcse_model
-tokenizer,bert_model,simcse_model = get_mod1()
+
+tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
+bert_model = AutoModel.from_pretrained(bert_model_name)
+simcse_model = AutoModel.from_pretrained(simcse_model_name)
 hidden_size = bert_model.config.hidden_size
 
 class CombinedModel(nn.Module):
@@ -83,14 +79,11 @@ class CombinedModel(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 num_labels = 3  # Adjust as needed
 mod = CombinedModel(bert_model, simcse_model, hidden_size, num_labels).to(device)
-@st.cache_resource
-def get_pth():
-    repo_id = "sanjayyy/combinedModel"
-    filename = "best_model_full.pth"
-    model_path = hf_hub_download(repo_id=repo_id, filename=filename)
-    checkpoint = torch.load(model_path, map_location='cpu')
-    return checkpoint
-checkpoint = get_pth()
+repo_id = "sanjayyy/combinedModel"
+filename = "best_model_full.pth"
+model_path = hf_hub_download(repo_id=repo_id, filename=filename)
+checkpoint = torch.load(model_path, map_location='cpu')
+
 # Check if the model is wrapped in DataParallel and extract the state_dict if necessary
 if isinstance(checkpoint, torch.nn.parallel.DataParallel):
     checkpoint = checkpoint.module.state_dict()
@@ -98,11 +91,8 @@ if isinstance(checkpoint, torch.nn.parallel.DataParallel):
 # Load the state dict
 mod.load_state_dict(checkpoint)
 mod.eval() 
-@st.cache(allow_output_mutation=True)
-def get_tok():
-    tokenizer = AutoTokenizer.from_pretrained('sanjayyy/newBert')  # Adjust if using a different tokenizer
-    return tokenizer
-tokenizer = get_tok()
+tokenizer = AutoTokenizer.from_pretrained('sanjayyy/newBert')  # Adjust if using a different tokenizer
+
 def predict_class(sentence):
     # Preprocess the input sentence
     inputs = tokenizer(sentence, return_tensors='pt', padding=True, truncation=True, max_length=128)
